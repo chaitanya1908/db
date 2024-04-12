7a85e743da5c43770db99ddecc2dde6c47e4447a
chaitanya1908 <155857194+chaitanya1908@users.noreply.github.com>
2024-04-11 21:21:18 -0500

    modify

--- new.py
+++ new.py
@@ -10,13 +10,10 @@ product_json = sys.argv[1]
 df = spark.read.json(product_json)
 columns_to_drop = ['ThumbNailPhoto', 'ThumbnailPhotoFileName']
 df = df.drop(*columns_to_drop)
-df_filtered = df.filter(df['_corrupt_record'].isNull())
-
-# Show the first 5 rows of the DataFrame
-df_filtered.show(5)
+df.printSchema()
 
 # Write the modified data to a new JSON file
 output_json_path = "NewSalesLT_Product_20200716.json"
-df_filtered.write.mode("overwrite").json(output_json_path)
+df.write.mode("overwrite").json(output_json_path)
 
 spark.stop()
